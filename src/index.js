7c12f12936ad0083b87464215c8b5255c5b31c7f
Darxan <azizera1992@yandex.ru>
2020-11-03 14:19:09 +0500

    live search users in github Rxjs application

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import { EMPTY, fromEvent } from 'rxjs';
+import {
+        debounceTime, 
+        distinctUntilChanged, 
+        map, 
+        switchMap, 
+        mergeMap, 
+        tap, 
+        catchError,
+        filter} from 'rxjs/operators'
+import {ajax} from 'rxjs/ajax'
+
+const url = "https://api.github.com/search/users?q=";
+
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+let resultCount = document.getElementById('result_count');
+
+
+const stream$ = fromEvent(search, 'input')
+.pipe(
+  map(e => e.target.value),
+  debounceTime(1000),
+  distinctUntilChanged(),
+  tap(() => result.innerHTML = ""),
+  filter(v => v.trim()),
+  switchMap(v => ajax.getJSON(url + v).pipe(
+    catchError(err => EMPTY)
+  )),
+  map(response => response.items), // array qilib oladi
+  // mergeMap(items => items) // xar birini aloxida olish uchun
+)
+
+
+stream$.subscribe(users => {
+console.log("users", users)
+const count = `<h4 class="center"> Results: ${users.length}.</h4>`;
+resultCount.innerHTML = count;
+//   const html = `
+//   <div class="card">
+//     <div class="card-image">
+//       <img src="${user.avatar_url}" />
+//       <span class="card-title" style="color: black"> ${user.login}</span>
+//     </div>
+//     <div class="card-action">
+//       <a href="${user.html_url}" class="waves-effect waves-light btn" target="_blank">Open account detail</a>
+//     </div>
+// </div>`
+users.map(user => {
+  const html =
+    `
+    <div class="col s12 m3">
+      <div class="card">
+        <div class="card-image">
+          <img src="${user.avatar_url}">
+          <span class="card-title">${user.login}</span>
+            <a class="btn-floating halfway-fab waves-effect waves-light red" 
+                href="${user.html_url}" target="_blank">
+            <i class="large material-icons">add</i></a>
+          </div>
+          <div class="card-content">
+            <p>${user.login}</p>
+          </div>
+        </div>
+      </div>`
+    result.insertAdjacentHTML('beforeend', html)
+})
+  
+})
---
+++ src/problem.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById('interval')
+const rxjsBtn = document.getElementById('rxjs')
+const display = document.querySelector('#problem .result')
+
+const people = [
+  {name: 'Aziz', age: 25},
+  {name: 'Elana', age: 17},
+  {name: 'Devid', age: 18},
+  {name: 'John', age: 14},
+  {name: 'Lisae', age: 32},
+  {name: 'Irinae', age: 23},
+  {name: 'Olegoom', age: 20}
+]
